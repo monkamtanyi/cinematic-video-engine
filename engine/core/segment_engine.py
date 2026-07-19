929b991898f49dd1e2292cc7cc940245a4917c07
monkamtanyi <monkamtanyi@gmail.com>
2026-07-19 05:41:20 -0400

    Set all cinematic segments to equal 25 percent duration

--- engine/core/segment_engine.py
+++ engine/core/segment_engine.py
@@ -6,12 +6,12 @@ class SegmentEngine:
     def resolve(self, i, total):
         p = i / max(1, total)
 
-        # LOCKED SEGMENTS (your final design)
-        if p < 0.50:
-            return 0   # Segment A
-        elif p < 0.65:
-            return 1   # Segment B
-        elif p < 0.80:
-            return 2   # Segment C
+        # TRUE 25% SEGMENT TIMELINE
+        if p < 0.25:
+            return 0   # Segment 1
+        elif p < 0.50:
+            return 1   # Segment 2
+        elif p < 0.75:
+            return 2   # Segment 3
         else:
-            return 3   # Segment D
+            return 3   # Segment 4
